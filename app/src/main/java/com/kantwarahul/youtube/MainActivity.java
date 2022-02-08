a80abc5622a94f2f7423f853ab69698bff624498
Rahul Kantwa <ce18b024@iittp.ac.in>
2022-02-08 23:23:09 +0530

    Done

---
+++ app/src/main/java/com/kantwarahul/youtube/MainActivity.java
@@ -0,0 +1,24 @@
+package com.kantwarahul.youtube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.jmedeisis.draglinearlayout.DragLinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DragLinearLayout dragLinearLayout = findViewById(R.id.layout_main);
+        for (int i = 0; i < dragLinearLayout.getChildCount(); i++)
+        {
+            View child = dragLinearLayout.getChildAt(i);
+            dragLinearLayout.setViewDraggable(child,child);
+        }
+    }
+}
