05e118baea530f3af8e5c9c2127910661198a40c
Rahul Kantwa <ce18b024@iittp.ac.in>
2022-02-10 11:45:26 +0530

    Done

--- app/src/main/java/com/kantwarahul/youtube/MainActivity.java
+++ app/src/main/java/com/kantwarahul/youtube/MainActivity.java
@@ -1,32 +1,30 @@
 package com.kantwarahul.youtube;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PictureInPictureParams;
 import android.content.Intent;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.util.Rational;
+import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.jmedeisis.draglinearlayout.DragLinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(getApplicationContext(),SecondActivity.class));
-            }
-        }, 1000);
-
+       startActivity(new Intent(getApplicationContext(), SecondActivity.class));
     }
+
 }
--- app/src/main/java/com/kantwarahul/youtube/SecondActivity.java
+++ app/src/main/java/com/kantwarahul/youtube/SecondActivity.java
@@ -1,23 +1,39 @@
 package com.kantwarahul.youtube;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PictureInPictureParams;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Rational;
+import android.view.Display;
 import android.view.View;
-
-import com.jmedeisis.draglinearlayout.DragLinearLayout;
+import android.widget.ImageView;
 
 public class SecondActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        DragLinearLayout dragLinearLayout = findViewById(R.id.layout_main);
-        for (int i = 0; i < dragLinearLayout.getChildCount(); i++)
-        {
-            View child = dragLinearLayout.getChildAt(i);
-            dragLinearLayout.setViewDraggable(child,child);
-        }
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onPause() {
+        super.onPause();
+        Display display = getWindowManager().getDefaultDisplay();
+        Point p = new Point();
+        display.getSize(p);
+        int width = p.x;
+        int height = p.y;
+
+        Rational rational = new Rational(width, height);
+
+        PictureInPictureParams.Builder builder = new PictureInPictureParams.Builder();
+        builder.setAspectRatio(rational).build();
+        enterPictureInPictureMode(builder.build());
     }
 }
